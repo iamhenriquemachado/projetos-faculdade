b2b1e366c8b74f67c51b2c9e09925532b32b6e4a
henrique.castro <heyhenriquecastro@gmail.com>
2025-09-20 13:47:56 -0300

    change: alteracao na estrtura de nomeacao das pastas

--- api/main.py
+++ api/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
-from router import transaction
+from controller import transaction_controller
 
 
 app = FastAPI()
 router = APIRouter()
 API_PORT = 3000
 
-app.include_router(transaction.router, prefix="/api", tags=["api"])
+app.include_router(transaction_controller.router, prefix="/api", tags=["api"])
 
 
 if __name__ == "__main__":
